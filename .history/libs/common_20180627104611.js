91171447a1cb4f7685f56f1a653744371b4f33a3
xuewenjie <xuewenjie@beecredit.com>
2018-09-25 18:32:23 +0800

    no message

--- .history/libs/common_20180627104611.js
+++ .history/libs/common_20180627104611.js
@@ -4,7 +4,9 @@ module.exports={
   MD5_SUFFIX: 'FDSW$t34tregt5tO&$(#RHuyoyiUYE*&OI$HRLuy87odlfh)',
   md5: function (str){
     var obj=crypto.createHash('md5');
+
     obj.update(str);
+
     return obj.digest('hex');
   }
 };
--- .history/libs/common_20180627104618.js
+++ .history/libs/common_20180627104618.js
@@ -5,6 +5,7 @@ module.exports={
   md5: function (str){
     var obj=crypto.createHash('md5');
     obj.update(str);
+
     return obj.digest('hex');
   }
 };
